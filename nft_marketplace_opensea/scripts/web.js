3366a58588856ed865db45d89f91a96b6bd0f4e0
nehay <nehay.w@gmail.com>
2022-07-11 11:35:12 +0530

    Updated design of marketplace

---
+++ nft_marketplace_opensea/scripts/web.js
@@ -0,0 +1,41 @@
+$('#wallet').click(function(e){
+  // web3Handler()
+  if (typeof window.ethereum !== 'undefined') {
+    window.ethereum
+    .request({
+      method: "eth_requestAccounts",
+    })
+    .then((accounts) => {
+      console.log(accounts[0]);
+      $('#account').val(accounts[0])
+    })
+    .catch((error) => {
+      console(error)
+      alert("Something went wrong");
+    });
+   } 
+})
+
+
+// $(".purchase").click(function(){
+//   debugger;
+//     let data = {itemId: $("itemId").text()};
+//     fetch("/purchase", {
+//       method: "POST",
+//       headers: {'Content-Type': 'application/json'}, 
+//       body: JSON.stringify(data)
+//     })
+// });
+
+
+$(".purchase").click(function(){
+  debugger;
+    data = {
+      'itemId' : parseInt($(this).parent().find("small").html()),
+    }
+    $.ajax({url: "/purchase", method :'POST', data : data,success: function(result){
+      debugger;
+      // alert(result)
+    }});
+
+});
